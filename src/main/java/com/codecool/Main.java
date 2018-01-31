5406dd95999f711c4b5f2db25aabdc9be9341e1d
GaborMartin <gabormartin94@gmail.com>
2018-01-31 14:14:55 +0100

    Fixed method calls in class Main

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -5,12 +5,12 @@ public class Main {
         PersistentStore ps = new PersistentStore();
         StoreManager manager = new StoreManager();
         manager.addStorage(ps);
-        //manager.addCDProduct("Master of puppets", 1500, 10);
-        //manager.addBookProduct("Bölcsek Köve", 2500, 500);
-        //ps.store();
+        manager.addCDProduct("Master of puppets", 1500, 10);
+        manager.addBookProduct("Bölcsek Köve", 2500, 500);
+        manager.addCDProduct("Akkezdet", 5000, 12);
+        manager.addBookProduct("Csicskák notesze", 500, 5);
+        ps.store();
         ps.loadProducts();
-        //Product metallica = new CDProduct("Master of puppets", 1500, 10);
-        //Product harrypotter = new BookProduct("Bölcsek Köve", 2500, 500);
         System.out.println(manager.listProducts());
         System.out.println(manager.getTotalProductPrice());
 
