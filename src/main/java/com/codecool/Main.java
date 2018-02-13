a798a155bda90571052160c76361b3f159fb8f81
GaborMartin <gabormartin94@gmail.com>
2018-02-13 13:25:49 +0100

    Modified products in Main

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -8,8 +8,9 @@ public class Main {
         manager.addStorage(csvPs);
         manager.addCDProduct("Master of puppets", 1500, 10);
         manager.addBookProduct("Bölcsek Köve", 2500, 500);
-        manager.addCDProduct("Akkezdet", 5000, 12);
-        manager.addBookProduct("Csicskák notesze", 500, 5);
+        manager.addCDProduct("Four horsemen", 5000, 12);
+        manager.addBookProduct("Black", 500, 5);
+        manager.addCDProduct("Infected Mushroom II", 5000, 15);
         csvPs.store("Products.csv");
         ps.loadProducts("Products.xml");
         System.out.println(manager.listProducts());
