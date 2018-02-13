9b90c57a8dde1a9391d3f252a22a81bbf025fb76
GaborMartin <gabormartin94@gmail.com>
2018-02-13 10:37:48 +0100

    Added new csvPersistenStore instance to storeManager

--- src/main/java/com/codecool/CsvStore.java
+++ src/main/java/com/codecool/CsvStore.java
@@ -1,4 +1,87 @@
 package com.codecool;
 
-public class CsvStore {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class CsvStore implements StorageCapable {
+
+    private List<Product> products = new ArrayList<>();
+
+    protected abstract void storeProduct(Product product);
+
+    private final String CSV_SEPARATOR = ",";
+
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    protected Product createProduct(String type, String name, int price, int size) {
+        Product product = null;
+        try {
+            if (type.toLowerCase().equals("book")) {
+                product = new BookProduct(name, price, size);
+            } else if (type.toLowerCase().equals("cd")) {
+                product = new CDProduct(name, price, size);
+            }
+
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+            System.out.println("Not valid type! (Try cd or book!)");
+        }
+        return product;
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return products;
+    }
+
+    @Override
+    public void storeCDProduct(String name, int price, int tracks) {
+        storeProduct(createProduct("book", name, price, tracks));
+    }
+
+    @Override
+    public void storeBookProduct(String name, int price, int pages) {
+        storeProduct(createProduct("book", name, price, pages));
+    }
+
+    public void saveToCsv(String fileName) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"));
+            StringBuffer header = new StringBuffer();
+            header.append("Name");
+            header.append(CSV_SEPARATOR);
+            header.append("Price");
+            header.append(CSV_SEPARATOR);
+            header.append("Pages/Tracks");
+            bw.write(header.toString());
+            bw.newLine();
+            for (Product product : products) {
+                StringBuffer oneLine = new StringBuffer();
+                oneLine.append(product.getName());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(product.getPrice());
+                oneLine.append(CSV_SEPARATOR);
+                if (product instanceof CDProduct) {
+                    oneLine.append(((CDProduct) product).getNumOfTracks());
+                } else if (product instanceof BookProduct) {
+                    oneLine.append(((BookProduct) product).getNumOfPage());
+                }
+                bw.write(oneLine.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+
+        } catch (IOException e) {
+        }
+    }
+
+    public void store(String filename){
+        saveToCsv(filename);
+    }
 }
--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -3,13 +3,14 @@ package com.codecool;
 public class Main {
     public static void main(String[] args) {
         PersistentStore ps = new PersistentStore();
+        CsvPersistentStore csvPs = new CsvPersistentStore();
         StoreManager manager = new StoreManager();
-        manager.addStorage(ps);
+        manager.addStorage(csvPs);
         manager.addCDProduct("Master of puppets", 1500, 10);
         manager.addBookProduct("Bölcsek Köve", 2500, 500);
         manager.addCDProduct("Akkezdet", 5000, 12);
         manager.addBookProduct("Csicskák notesze", 500, 5);
-        ps.store("Products.xml");
+        csvPs.store("Products.csv");
         ps.loadProducts("Products.xml");
         System.out.println(manager.listProducts());
         System.out.println(manager.getTotalProductPrice());
